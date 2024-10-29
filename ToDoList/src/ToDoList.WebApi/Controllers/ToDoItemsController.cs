13e32ba231ff38f67970de12a8180d914592d873
MarketaHorakova <99902734+MarketaHorakova@users.noreply.github.com>
2024-10-29 11:02:33 +0100

    Initial commit

---
+++ ToDoList/src/ToDoList.WebApi/Controllers/ToDoItemsController.cs
@@ -0,0 +1,129 @@
+namespace ToDoList.WebApi.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Domain.DTOs;
+using ToDoList.Domain.Models;
+using ToDoList.Persistence;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ToDoItemsController : ControllerBase
+{
+    public readonly List<ToDoItem> items = []; // po dopsání úkolu již není potřeba a můžeme tento field smazat ;)
+    private readonly ToDoItemsContext context;
+    public ToDoItemsController(ToDoItemsContext context)
+    {
+        this.context = context;
+    }
+
+    [HttpPost]
+    public ActionResult<ToDoItemGetResponseDto> Create(ToDoItemCreateRequestDto request)
+    {
+        //map to Domain object as soon as possible
+        var item = request.ToDomain();
+
+        //try to create an item
+        try
+        {
+            context.ToDoItems.Add(item);
+            context.SaveChanges();
+        }
+        catch (Exception ex)
+        {
+            return Problem(ex.Message, null, StatusCodes.Status500InternalServerError); //500
+        }
+
+        //respond to client
+        return CreatedAtAction(
+            nameof(ReadById),
+            new { toDoItemId = item.ToDoItemId },
+            ToDoItemGetResponseDto.FromDomain(item)); //201
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<ToDoItemGetResponseDto>> Read()
+    {
+        List<ToDoItem> itemsToGet;
+        try
+        {
+            itemsToGet = items;
+        }
+        catch (Exception ex)
+        {
+            return Problem(ex.Message, null, StatusCodes.Status500InternalServerError); //500
+        }
+
+        //respond to client
+        return (itemsToGet is null || itemsToGet.Count is 0)
+            ? NotFound() //404
+            : Ok(itemsToGet.Select(ToDoItemGetResponseDto.FromDomain)); //200
+    }
+
+    [HttpGet("{toDoItemId:int}")]
+    public ActionResult<ToDoItemGetResponseDto> ReadById(int toDoItemId)
+    {
+        //try to retrieve the item by id
+        ToDoItem? itemToGet;
+        try
+        {
+            itemToGet = items.Find(i => i.ToDoItemId == toDoItemId);
+        }
+        catch (Exception ex)
+        {
+            return Problem(ex.Message, null, StatusCodes.Status500InternalServerError); //500
+        }
+
+        //respond to client
+        return (itemToGet is null)
+            ? NotFound() //404
+            : Ok(ToDoItemGetResponseDto.FromDomain(itemToGet)); //200
+    }
+
+    [HttpPut("{toDoItemId:int}")]
+    public IActionResult UpdateById(int toDoItemId, [FromBody] ToDoItemUpdateRequestDto request)
+    {
+        //map to Domain object as soon as possible
+        var updatedItem = request.ToDomain();
+
+        //try to update the item by retrieving it with given id
+        try
+        {
+            //retrieve the item
+            var itemIndexToUpdate = items.FindIndex(i => i.ToDoItemId == toDoItemId);
+            if (itemIndexToUpdate == -1)
+            {
+                return NotFound(); //404
+            }
+            updatedItem.ToDoItemId = toDoItemId;
+            items[itemIndexToUpdate] = updatedItem;
+        }
+        catch (Exception ex)
+        {
+            return Problem(ex.Message, null, StatusCodes.Status500InternalServerError); //500
+        }
+
+        //respond to client
+        return NoContent(); //204
+    }
+
+    [HttpDelete("{toDoItemId:int}")]
+    public IActionResult DeleteById(int toDoItemId)
+    {
+        //try to delete the item
+        try
+        {
+            var itemToDelete = items.Find(i => i.ToDoItemId == toDoItemId);
+            if (itemToDelete is null)
+            {
+                return NotFound(); //404
+            }
+            items.Remove(itemToDelete);
+        }
+        catch (Exception ex)
+        {
+            return Problem(ex.Message, null, StatusCodes.Status500InternalServerError);
+        }
+
+        //respond to client
+        return NoContent(); //204
+    }
+}
