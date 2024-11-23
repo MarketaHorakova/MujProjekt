d35346a7f9bf51c0c40783f2e97f366b13d411ba
Markéta Horáková <marketa.horakova@thermofisher.com>
2024-11-23 11:30:56 +0100

    Update 9 lesson

---
+++ ToDoList/src/ToDoList.Frontend/Clients/IToDoItemsClient.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDoList.Frontend.Views;
+
+namespace ToDoList.Frontend.Clients;
+
+
+public interface IToDoItemsClient
+{
+    public Task<List<ToDoItemView>> ReadItemsAsync();
+
+}
+
---
+++ ToDoList/src/ToDoList.Frontend/Clients/ToDoItemsClient.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDoList.Domain.DTOs;
+using ToDoList.Frontend.Views;
+
+namespace ToDoList.Frontend.Clients;
+public class ToDoItemsClient(HttpClient httpClient) : IToDoItemsClient
+{
+    public async Task<List<ToDoItemView>> ReadItemsAsync()
+    {
+        var toDoItemsView = new List<ToDoItemView>();
+        var response = await httpClient.GetFromJsonAsync<List<ToDoItemGetResponseDto>>("api/ToDoItems");
+        toDoItemsView = response.Select(dto => new ToDoItemView
+        {
+            ToDoItemId = dto.Id,
+            Name = dto.Name,
+            Description = dto.Description,
+            IsCompleted = dto.IsCompleted
+        }).ToList();
+        return toDoItemsView;
+    }
+}
